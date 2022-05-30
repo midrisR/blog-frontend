1b46c098fdb361ee71a284c378964ad016f0ae17
midrisR <midrisramdhani96@gmail.com>
2022-05-30 23:39:22 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../styles/style.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<div className='bg-slate-900 min-h-screen'>
+			<Component {...pageProps} />
+		</div>
+	);
 }
 
-export default MyApp
+export default MyApp;
