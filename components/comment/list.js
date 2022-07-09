329bc4b07b0ab8ac9dcef5fca1d8fdcadf75b1fc
midrisR <midrisramdhani96@gmail.com>
2022-07-09 21:18:41 +0700

    idk2

--- components/comment/list.js
+++ components/comment/list.js
@@ -1,34 +1,38 @@
+import Image from 'next/image';
 export default function CommentList({ comments }) {
-	if (!comments) return null;
 	return (
-		<div className="space-y-6 mt-10">
-			{comments.map((comment, i) => {
-				return (
-					<div key={i} className="flex space-x-4">
-						<div className="flex-shrink-0">
-							<img
-								src={comment.guest.image}
-								alt={comment.guest.name}
-								width={40}
-								height={40}
-								className="rounded-full"
-							/>
-						</div>
-
-						<div className="flex-grow">
-							<div className="flex space-x-2">
-								<span className="text-sm text-white font-semibold">
-									{comment.guest.name}
-								</span>
-								<span className="text-white/25 text-sm ml-4">
-									{new Date(comment.created_at).toDateString('id')}
-								</span>
+		<>
+			{comments && (
+				<div className="space-y-6 mt-10">
+					{comments.map((comment, i) => {
+						return (
+							<div key={i} className="flex space-x-4">
+								<div className="flex-shrink-0">
+									<Image
+										loading="lazy"
+										src={comment.guest.image}
+										alt={comment.guest.name}
+										width={40}
+										height={40}
+										className="rounded-full"
+									/>
+								</div>
+								<div className="flex-grow">
+									<div className="flex space-x-2">
+										<span className="text-sm text-white font-semibold">
+											{comment.guest.name}
+										</span>
+										<span className="text-white/25 text-sm ml-4">
+											{new Date(comment.created_at).toDateString('id')}
+										</span>
+									</div>
+									<div className="text-sm text-white">{comment.comment}</div>
+								</div>
 							</div>
-							<div className="text-sm text-white">{comment.comment}</div>
-						</div>
-					</div>
-				);
-			})}
-		</div>
+						);
+					})}
+				</div>
+			)}
+		</>
 	);
 }
--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ const withTM = require('next-transpile-modules')(['react-syntax-highlighter']);
 module.exports = {
 	withTM,
 	images: {
-		domains: ['images.unsplash.com', 'localhost', 'res.cloudinary.com'],
+		domains: [
+			'images.unsplash.com',
+			'localhost',
+			'res.cloudinary.com',
+			'avatars.githubusercontent.com',
+			'platform-lookaside.fbsbx.com',
+			'pbs.twimg.com',
+		],
 	},
 	compiler: {
 		styledComponents: true,
