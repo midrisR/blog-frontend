8f8039499644e3ee73ef923673b5423148198edc
midrisR <midrisramdhani96@gmail.com>
2022-07-09 17:35:44 +0700

    add twitter provider login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import FacebookProvider from 'next-auth/providers/facebook';
+import TwitterProvider from 'next-auth/providers/twitter';
 
 export default NextAuth({
 	providers: [
@@ -11,6 +12,11 @@ export default NextAuth({
 		FacebookProvider({
 			clientId: process.env.FACEBOOK_CLIENT_ID,
 			clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+		}),
+		TwitterProvider({
+			clientId: process.env.TWITTER_CLIENT_ID,
+			clientSecret: process.env.TWITTER_CLIENT_SECRET,
+			version: '2.0',
 		}),
 	],
 	callbacks: {
