0e7f51d55f1c2c5ee4ac2348ad3b7ec016b280fe
midrisR <midrisramdhani96@gmail.com>
2022-07-09 16:34:42 +0700

    fix conflic package

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,6 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import FacebookProvider from 'next-auth/providers/facebook';
-import { MongoClient } from 'mongodb';
 
 export default NextAuth({
 	providers: [
