a58f3d48c4f59e1a29f58a5241f6eb516821b1b2
midrisR <midrisramdhani96@gmail.com>
2022-07-11 20:58:53 +0700

    edit config

--- next.config.js
+++ next.config.js
@@ -2,22 +2,20 @@ const withTM = require('next-transpile-modules')(['react-syntax-highlighter']);
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
 	enabled: process.env.ANALYZE === 'true',
 });
-module.exports = (phase, defaultConfig) => {
-	return withBundleAnalyzer(defaultConfig, {
-		withTM,
-		images: {
-			domains: [
-				'images.unsplash.com',
-				'localhost',
-				'res.cloudinary.com',
-				'avatars.githubusercontent.com',
-				'platform-lookaside.fbsbx.com',
-				'pbs.twimg.com',
-			],
-		},
-		compiler: {
-			styledComponents: true,
-		},
-		reactStrictMode: true,
-	});
-};
+module.exports = withBundleAnalyzer({
+	withTM,
+	images: {
+		domains: [
+			'images.unsplash.com',
+			'localhost',
+			'res.cloudinary.com',
+			'avatars.githubusercontent.com',
+			'platform-lookaside.fbsbx.com',
+			'pbs.twimg.com',
+		],
+	},
+	compiler: {
+		styledComponents: true,
+	},
+	reactStrictMode: true,
+});
